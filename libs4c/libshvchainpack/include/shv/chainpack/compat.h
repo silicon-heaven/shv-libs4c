e0c2b4d26a99b6ba1ca26c5e6e5f007e5b6907ce
Václav Kubernát <vkubernat@elektroline.cz>
2024-10-23 15:08:46 +0200

    Fix compat file

--- libs4c/libshvchainpack/include/shv/chainpack/compat.h
+++ libs4c/libshvchainpack/include/shv/chainpack/compat.h
@@ -1,4 +1,11 @@
 #pragma once
+#ifdef __has_include
+	#if __has_include(<unistd.h>)
+		#include <unistd.h>
+	#endif
+#endif
+
 #ifndef __ssize_t_defined
-#define ssize_t int64_t
+#include <stdint.h>
+typedef intmax_t ssize_t;
 #endif
