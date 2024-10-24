378216ee772ad52daf4ba24f2ef30e1eb226f9a2
Václav Kubernát <vkubernat@elektroline.cz>
2024-10-24 09:52:52 +0200

    compat: Define ssize_t only on Windows
    I give up. I tried detecting if ssize_t was defined, but there's really no
    standard way (e.g. NuttX doesn't define __ssize_t_defined. We need it for
    Windows only anyway.

--- libs4c/libshvchainpack/include/shv/chainpack/compat.h
+++ libs4c/libshvchainpack/include/shv/chainpack/compat.h
@@ -1,11 +1,5 @@
 #pragma once
-#ifdef __has_include
-	#if __has_include(<unistd.h>)
-		#include <unistd.h>
-	#endif
-#endif
-
-#ifndef __ssize_t_defined
+#ifdef _MSC_VER
 #include <stdint.h>
 typedef intmax_t ssize_t;
 #endif
