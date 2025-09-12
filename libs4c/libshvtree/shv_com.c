fe7f266a1a82201d929a7d184b17b331e028ddac
Pavel Pisa <ppisa@pikron.com>
2025-09-12 09:58:03 +0200

    shv_com_destroy: do not shv_tree_destroy, there can be more communication contexts for the one
    Even if the tree should be destroyed, then the its destroy
    cannot be started when the communication process accesses it still.
    
    Signed-off-by: Pavel Pisa <ppisa@pikron.com>

--- libs4c/libshvtree/shv_com.c
+++ libs4c/libshvtree/shv_com.c
@@ -1194,7 +1194,6 @@ shv_con_ctx_t *shv_com_init(struct shv_node *root, struct shv_connection *connec
 void shv_com_destroy(shv_con_ctx_t *shv_ctx)
 {
     atomic_store(&shv_ctx->running, false);
-    shv_tree_destroy(shv_ctx->root);
     shv_stop_process_thread(shv_ctx);
     free(shv_ctx);
 }
